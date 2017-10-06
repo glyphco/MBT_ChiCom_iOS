82c847b6da40df822bea170d3b3f0578f8fb35bf
ARO <aro@aros-macbook-pro.local>
2017-10-06 14:41:59 -0500

    Initial Commit

---
+++ ChicagoComedy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ChicagoComedy
+//
+//  Created by ARO on 10/6/17.
+//  Copyright © 2017 MBT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
